b8bd68e1f865021c5506fcb3f820f2f62138e729
HYXiaoHai <21991579@qq.com>
2025-10-04 20:04:13 +0800

    practice1
    map AVLTree practice

---
+++ 10.4test/10.4test/test.cpp
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<map>
+using namespace std;
+
+void test01()
+{
+
+}
+
+int main()
+{
+	// initializer_list构造及迭代遍历 
+	map<string, string> dict = { {"left", "左边"}, {"right", "右边"},
+   {"insert", "插⼊"},{ "string", "字符串" } };
+	//map<string, string>::iterator it = dict.begin();
+
+	auto it = dict.begin();
+	while (it != dict.end())
+	{
+		//cout << (*it).first <<":"<<(*it).second << endl;
+		// map的迭代基本都使⽤operator->,这⾥省略了⼀个-> 
+		// 第⼀个->是迭代器运算符重载，返回pair*，第⼆个箭头是结构指针解引⽤取pair数据
+		//cout << it.operator->()->first << ":" << it.operator->()-> second << endl;
+		cout << it->first << ":" << it->second << endl;
+		++it;
+	}
+	cout << endl;
+
+	//insert插⼊pair对象的4种⽅式，对⽐之下，最后⼀种最⽅便 
+	pair<string, string> kv1("first", "第⼀个");
+	dict.insert(kv1);
+	dict.insert(pair<string, string>("second", "第⼆个"));
+	dict.insert(make_pair("sort", "排序"));
+	dict.insert({ "auto", "⾃动的" });
+
+	//"left"已经存在，插⼊失败 
+	dict.insert({ "left", "左边，剩余" });
+	dict.insert({ "left", "*****" });//插入失败
+	dict.insert({ "Left", "左边" });//插入成功 map的插入只看key
+
+	auto it = dict.find("first");//查找
+	it->second = "第二";
+	dict["set"];//插入
+	dict["set"] = "集合";//修改
+	dict["begin"] = "开始";//插入+修改
+	cout << dict["set"] << endl;//查找
+
+	// 范围for遍历 
+	for (const auto& e : dict)
+	{
+		cout << e.first << ":" << e.second << endl;
+	}
+	cout << endl;
+
+	string str;
+	while (cin >> str)
+	{
+		auto ret = dict.find(str);
+		if (ret != dict.end())
+		{
+			cout << "->" << ret->second << endl;
+		}
+		else
+		{
+			cout << "⽆此单词，请重新输⼊" << endl;
+		}
+	}
+	// erase等接⼝跟set完全类似，这⾥就不演⽰讲解了 
+	return 0;
+}
---
+++ AVLTRee/AVLTRee/AVLTree.h
@@ -0,0 +1,101 @@
+﻿#pragma once
+#include<iostream>
+#include<cassert>
+using namespace std;
+template<class K, class V>
+struct AVLTreeNode
+{
+	// 需要parent指针，后续更新平衡因⼦可以看到 
+	pair<K, V> _kv;
+	AVLTreeNode<K, V>* _left;
+	AVLTreeNode<K, V>* _right;
+	AVLTreeNode<K, V>* _parent;
+	int _bf; // 平衡因子
+	AVLTreeNode(const pair<K, V>& kv)
+		:_kv(kv)
+		, _left(nullptr)
+		, _right(nullptr)
+		, _parent(nullptr)
+		, _bf(0)
+	{ }
+};
+template<class K, class V>
+class AVLTree
+{
+	typedef AVLTreeNode<K, V> Node;
+public:
+	bool Insert(const pair<K, V>& kv)
+	{
+		if (_root == nullptr)
+		{
+			_root = new Node(kv);
+			return true;
+		}
+		Node* parent = nullptr;
+		Node* cur = _root;
+		while (cur)
+		{
+			if (cur->_kv.first < kv.first)
+			{
+				parent = cur;
+				cur = cur->_right;
+			}
+			else if (cur->_kv.first > kv.first)
+			{
+				parent = cur;
+				cur = cur->_left;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		cur = new Node(kv);
+		if (parent->_kv.first < kv.first)
+		{
+			parent->_right = cur;
+		}
+		else
+		{
+			parent->_left = cur;
+		}
+
+		cur->_parent = parent;
+
+		// 更新平衡因⼦ 
+		while (parent)
+		{
+			// 更新平衡因⼦ 
+			if (cur == parent->_left)
+				parent->_bf--;
+			else
+				parent->_bf++;
+			if (parent->_bf == 0)
+			{
+				// 更新结束 
+				break;
+			}
+			else if (parent->_bf == 1 || parent->_bf == -1)
+			{
+				// 继续往上更新 
+				cur = parent;
+				parent = parent->_parent;
+			}
+			else if (parent->_bf == 2 || parent->_bf == -2)
+			{
+				// 不平衡了，旋转处理 
+				break;
+			}
+			else
+			{
+				//插入之前这颗树就有2/-2bf的节点，这棵树之前就不是AVLTree
+				assert(false);
+			}
+		}
+		return true;
+	}
+
+private:
+	Node* _root = nullptr;
+};
