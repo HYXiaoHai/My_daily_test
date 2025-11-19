6725e59d5aa73a79957aa722551acd021bf1e279
HYXiaoHai <21991579@qq.com>
2025-11-19 21:51:57 +0800

    11.9

--- 11.19/11.19/test.cpp
+++ 11.19/11.19/test.cpp
@@ -312,66 +312,66 @@ using namespace std;
 //	return 0;
 //}
 
-double Divide(int a, int b)
-{
-	// 当b == 0时抛出异常 
-	if (b == 0)
-	{
-		throw "Divide by zero condition!";
-	}
-	else
-	{
-		return (double)a / (double)b;
-	}
-}
-void Func()
-{
-	// 这⾥可以看到如果发⽣除0错误抛出异常，另外下⾯的array和array2没有得到释放。 
-	// 所以这⾥捕获异常后并不处理异常，异常还是交给外⾯处理，这⾥捕获了再重新抛出去。 
-	// 但是如果array2new的时候抛异常呢，就还需要套⼀层捕获释放逻辑，这⾥更好解决⽅案 
-	// 是智能指针，否则代码太戳了 
-	int* array1 = new int[10];
-	int* array2 = new int[10]; // 抛异常呢 
-	try
-	{
-		int len, time;
-		cin >> len >> time;
-		cout << Divide(len, time) << endl;
-	}
-	catch (...)
-	{
-		cout << "delete []" << array1 << endl;
-		cout << "delete []" << array2 << endl;
-		delete[] array1;
-		delete[] array2;
-		throw; // 异常重新抛出，捕获到什么抛出什么 
-	}
-	// ...
-	cout << "delete []" << array1 << endl;
-	delete[] array1;
-	cout << "delete []" << array2 << endl;
-	delete[] array2;
-}
-int main()
-{
-	try
-	{
-		Func();
-	}
-	catch (const char* errmsg)
-	{
-		cout << errmsg << endl;
-	}
-	catch (const exception& e)
-	{
-		cout << e.what() << endl;
-	}
-	catch (...)
-	{
-		cout << "未知异常" << endl;
-	}
-	return 0;
-}
+//double Divide(int a, int b)
+//{
+//	// 当b == 0时抛出异常 
+//	if (b == 0)
+//	{
+//		throw "Divide by zero condition!";
+//	}
+//	else
+//	{
+//		return (double)a / (double)b;
+//	}
+//}
+//void Func()
+//{
+//	// 这⾥可以看到如果发⽣除0错误抛出异常，另外下⾯的array和array2没有得到释放。 
+//	// 所以这⾥捕获异常后并不处理异常，异常还是交给外⾯处理，这⾥捕获了再重新抛出去。 
+//	// 但是如果array2new的时候抛异常呢，就还需要套⼀层捕获释放逻辑，这⾥更好解决⽅案 
+//	// 是智能指针，否则代码太戳了 
+//	int* array1 = new int[10];
+//	int* array2 = new int[10]; // 抛异常呢 
+//	try
+//	{
+//		int len, time;
+//		cin >> len >> time;
+//		cout << Divide(len, time) << endl;
+//	}
+//	catch (...)
+//	{
+//		cout << "delete []" << array1 << endl;
+//		cout << "delete []" << array2 << endl;
+//		delete[] array1;
+//		delete[] array2;
+//		throw; // 异常重新抛出，捕获到什么抛出什么 
+//	}
+//	// ...
+//	cout << "delete []" << array1 << endl;
+//	delete[] array1;
+//	cout << "delete []" << array2 << endl;
+//	delete[] array2;
+//}
+//int main()
+//{
+//	try
+//	{
+//		Func();
+//	}
+//	catch (const char* errmsg)
+//	{
+//		cout << errmsg << endl;
+//	}
+//	catch (const exception& e)
+//	{
+//		cout << e.what() << endl;
+//	}
+//	catch (...)
+//	{
+//		cout << "未知异常" << endl;
+//	}
+//	return 0;
+//}
 
 //template<class T>
 //class SmartPtr
@@ -440,8 +440,7 @@ int main()
 //		cout << errmsg << endl;
 //	}
 //	catch (const exception& e)
-//	{
-//		cout << e.what() << endl;
+ //		cout << e.what() << endl;
 //	}
 //	catch (...)
 //	{
@@ -449,3 +448,120 @@ int main()
 //	}
 //	return 0;
 //}
+struct Date
+{
+	int _year;
+	int _month;
+	int _day;
+	Date(int year = 1, int month = 1, int day = 1)
+		:_year(year)
+		, _month(month)
+		, _day(day)
+	{
+	}
+	~Date()
+	{
+		cout << "~Date()" << endl;
+	}
+};
+int main()
+{
+	auto_ptr<Date> ap1(new Date);
+	// 拷⻉时，管理权限转移，被拷⻉对象ap1悬空 
+	auto_ptr<Date> ap2(ap1);
+	// 空指针访问，ap1对象已经悬空 
+	//ap1->_year++;
+	unique_ptr<Date> up1(new Date);
+	// 不⽀持拷⻉ 
+	//unique_ptr<Date> up2(up1);
+	// ⽀持移动，但是移动后up1也悬空，所以使⽤移动要谨慎 
+	unique_ptr<Date> up3(move(up1));
+	shared_ptr<Date> sp1(new Date);
+	// ⽀持拷⻉ 
+	shared_ptr<Date> sp2(sp1);
+	shared_ptr<Date> sp3(sp2);
+	cout << sp1.use_count() << endl;
+	sp1->_year++;
+	cout << sp1->_year << endl;
+	cout << sp2->_year << endl;
+	cout << sp3->_year << endl;
+	// ⽀持移动，但是移动后sp1也悬空，所以使⽤移动要谨慎 
+	shared_ptr<Date> sp4(move(sp1));
+	return 0;
+}template<class T>
+void DeleteArrayFunc(T* ptr)
+{
+	delete[] ptr;
+}
+template<class T>
+class DeleteArray
+{
+public:
+	void operator()(T* ptr)
+	{
+		delete[] ptr;
+	}
+};
+class Fclose
+{
+public:
+	void operator()(FILE* ptr)
+	{
+		cout << "fclose:" << ptr << endl;
+		fclose(ptr);
+	}
+};
+int main()
+{
+	// 这样实现程序会崩溃 
+	// unique_ptr<Date> up1(new Date[10]);
+	// shared_ptr<Date> sp1(new Date[10]);
+	// 解决⽅案1 
+	// 因为new[]经常使⽤，所以unique_ptr和shared_ptr 
+	// 实现了⼀个特化版本，这个特化版本析构时⽤的delete[] 
+	unique_ptr<Date[]> up1(new Date[5]);
+	shared_ptr<Date[]> sp1(new Date[5]);
+	// 解决⽅案2 
+
+	// 仿函数对象做删除器 
+	//unique_ptr<Date, DeleteArray<Date>> up2(new Date[5], DeleteArray<Date>());
+	// unique_ptr和shared_ptr⽀持删除器的⽅式有所不同 
+	// unique_ptr是在类模板参数⽀持的，shared_ptr是构造函数参数⽀持的 
+	// 这⾥没有使⽤相同的⽅式还是挺坑的 
+	// 使⽤仿函数unique_ptr可以不在构造函数传递，因为仿函数类型构造的对象直接就可以调⽤ 
+	// 但是下⾯的函数指针和lambda的类型不可以 
+	unique_ptr<Date, DeleteArray<Date>> up2(new Date[5]);
+	shared_ptr<Date> sp2(new Date[5], DeleteArray<Date>());
+	// 函数指针做删除器  
+	unique_ptr<Date, void(*)(Date*)> up3(new Date[5], DeleteArrayFunc<Date>);
+	shared_ptr<Date> sp3(new Date[5], DeleteArrayFunc<Date>);
+	// lambda表达式做删除器 
+	auto delArrOBJ = [](Date* ptr) {delete[] ptr; };
+	unique_ptr<Date, decltype(delArrOBJ)> up4(new Date[5], delArrOBJ);
+	shared_ptr<Date> sp4(new Date[5], delArrOBJ);
+	// 实现其他资源管理的删除器 
+	shared_ptr<FILE> sp5(fopen("Test.cpp", "r"), Fclose());
+	shared_ptr<FILE> sp6(fopen("Test.cpp", "r"), [](FILE* ptr) {
+		cout << "fclose:" << ptr << endl;
+		fclose(ptr);
+		});
+	return 0;
+}
+int main()
+{
+	shared_ptr<Date> sp1(new Date(2024, 9, 11));
+	shared_ptr<Date> sp2 = make_shared<Date>(2024, 9, 11);
+	auto sp3 = make_shared<Date>(2024, 9, 11);
+	shared_ptr<Date> sp4;
+	// if (sp1.operator bool())
+	if (sp1)
+		cout << "sp1 is not nullptr" << endl;
+	if (!sp4)
+		cout << "sp1 is nullptr" << endl;
+
+	// 报错 
+	shared_ptr<Date> sp5 = new Date(2024, 9, 11);
+	unique_ptr<Date> sp6 = new Date(2024, 9, 11);
+
+	return 0;
+}
